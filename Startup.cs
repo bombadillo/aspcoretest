4af0362a8cc0c929fe358b0c1b6ad119d8076f9b
bombadillo <mumfordnsons@gmail.com>
2016-05-25 14:36:35 +0100

    Add config logic

--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,32 @@
 namespace AspCoreTest
 {
-    using System;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     
     using Services;
     
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+        
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+                
+            Configuration = builder.Build();
+        }        
         
         public void ConfigureServices(IServiceCollection services)
         { 
             services.AddMvcCore()
                     .AddJsonFormatters();
-                    
+                                                  
             services.AddTransient<IRetrieveProduct, ProductRetriever>();
+            services.AddSingleton<IConfigurationRoot>(sp => { return Configuration; });
         }
         
         public void Configure(IApplicationBuilder app)
